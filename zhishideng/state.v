b4cd4e7e53cf71e5f131a9ee4c85bed994a6091f
HuaJEE <756284528@qq.com>
2021-07-20 16:37:41 +0800

    V1.1
    摸了

--- zhishideng/state.v
+++ zhishideng/state.v
@@ -1,13 +1,15 @@
 //State Machine
-module state(brake,turn,clk,trans);
+module state(brake,turn,clk,led1,led2,led3);
 
 	input clk;//clock
 	input brake;//brake signal
 	input turn;//turn signal
 	
-	output wire trans;
+	output led1;
+	output led2;
+	output led3;
 	
-	reg [1:0]out;
+    reg [1:0]out;
 	
 	always@(posedge clk)
 	if(!brake && !turn)//no brake and turn signal
@@ -18,6 +20,24 @@ module state(brake,turn,clk,trans);
 		out <= 2'b10;
 	else if(brake && turn)//both brake and turn signal
 		out <= 2'b11;
-	trans = out;
+	
+case(out)//Different states of lights
+	00: led1 = 1'b0;
+		led2 = 1'b0;
+		led3 = 1'b0;
+	
+	01: led1 = 1'b1;
+		led2 = 1'b1;
+		led3 = 1'b1;
+		
+	10: led1 = 1'b1;
+		led2 = 1'b0;
+		led3 = 1'b1;
+		
+	11: led1 = 1'b1;
+		led2 = 1'b1;
+		led3 = 1'b1;
+	default;
+endcase
 
 endmodule
